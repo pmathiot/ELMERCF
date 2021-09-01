394c454ec985e5515730818e6285dab53d9222db
Pierre Mathiot <pierre.mathiot@univ-grenoble-alpes.fr>
2021-09-01 15:12:26 +0200

    add configuration files

---
+++ ANT50.GL1/ANT50.GL1-EPM009/run_param.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# number of elmer partition
+NP=48
+
+# number of HPC nodes
+NN=2
+
+# first iteration number (if more than 1, means restart i-1 are already in place)
+# end iteration
+STARTITER=1
+ENDITER=5
+
+# restart path and rst file (assume all in $IELMER)
+RSTINITpath=${IELMER}/RST/OPTIM_VEL_R15_48part
+RSTINITfile=OPTIM_R15_24to48.result
